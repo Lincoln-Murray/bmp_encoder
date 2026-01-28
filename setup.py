40d46799b2324981bc2aee88b6a47cb1d47774d6
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2026-01-28 08:17:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-print("Setup complete")
+from setuptools import setup
+setup(
+    name="bmp",
+    version="1.0",
+    description='Python BMP encoder.',
+    author='Lincoln Murray',
+    packages=["bmp_writer"]
+)
